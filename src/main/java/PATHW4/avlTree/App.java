c8c35bbd73ce5eafe27aef581eb0f4fc0fc96f1c
gsbsdj <90283102+gsbsdj@users.noreply.github.com>
2022-04-11 13:23:09 -0500

    initial commit for assignment

---
+++ src/main/java/PATHW4/avlTree/App.java
@@ -0,0 +1,13 @@
+package PATHW4.avlTree;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
